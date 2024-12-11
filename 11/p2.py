35d374dc4deef4fb06425c64b219a855b3dbeb49
Nick Muggio <nick@mugg.io>
2024-12-11 00:21:10 -0500

    Day 11

---
+++ 11/p2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+nums = {}
+
+def add(d, num, amt=1):
+  if num in d:
+    d[num] += amt
+  else:
+    d[num] = amt
+
+with open(sys.argv[1], mode="rt", encoding="utf-8") as file:
+  for num_str in file.readline().split():
+    num = int(num_str)
+    add(nums, num)
+
+for _ in range(75):
+  new_nums = {}
+
+  for num, count in nums.items():
+    if num == 0:
+      add(new_nums, 1, count)
+    else:
+      num_str = str(num)
+      if len(num_str) % 2 == 0:
+        a = int(num_str[:len(num_str) // 2])
+        b = int(num_str[len(num_str) // 2:])
+        add(new_nums, a, count)
+        add(new_nums, b, count)
+      else:
+        add(new_nums, num * 2024, count)
+
+  nums = new_nums
+
+print(sum(nums.values()))
